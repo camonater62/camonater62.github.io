47a773a9faba4cfa80e3314468e711a2c58b9140
Cameron West <camonater62@gmail.com>
2023-03-06 21:19:48 -0800

    New portfolio link + add p5

---
+++ background.js
@@ -0,0 +1,14 @@
+
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+    canvas.style('overflow', 'hidden');
+}
+
+function draw() {
+    clear(0);
+    translate(-width/2, -height/2);
+
+    // TODO
+}
